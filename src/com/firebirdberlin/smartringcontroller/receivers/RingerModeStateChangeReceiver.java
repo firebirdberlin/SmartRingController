02a23590516c7488c4bd2965c7aaf6f2beb430cb
Stefan Fruhner <Stefan.Fruhner@momox.biz>
2015-11-04 22:25:08 +0100

    Show a notification whenever the ringer mode changes to vibrate or silent.

--- src/com/firebirdberlin/smartringcontroller/receivers/RingerModeStateChangeReceiver.java
+++ src/com/firebirdberlin/smartringcontroller/receivers/RingerModeStateChangeReceiver.java
@@ -1,35 +1,72 @@
 package com.firebirdberlin.smartringcontrollerpro;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
 import android.media.AudioManager;
 import android.os.Bundle;
-import de.greenrobot.event.EventBus;
-import java.util.UUID;
-import org.json.JSONException;
-import org.json.JSONArray;
-import org.json.JSONObject;
+import android.os.Build;
+
 
 public class RingerModeStateChangeReceiver extends BroadcastReceiver {
     private final static String TAG = SmartRingController.TAG + ".RingerModeStateChangeReceiver";
-
+    private final static int NOTIFICATION_ID = 627;
+    private Context mContext;
     @Override
     public void onReceive(Context context, Intent intent) {
-        dumpIntent(intent);
-
+        if (intent == null) return;
+        mContext = context;
+        Bundle bundle = intent.getExtras();
+        int newRingerMode = bundle.getInt(AudioManager.EXTRA_RINGER_MODE, -1);
+        switch (newRingerMode) {
+            case AudioManager.RINGER_MODE_NORMAL:
+                cancelNotification();
+                break;
+            case AudioManager.RINGER_MODE_SILENT:
+                postNotification();
+                break;
+            case AudioManager.RINGER_MODE_VIBRATE:
+                postNotification();
+                break;
+        }
     }
 
-    public static void dumpIntent(Intent i){
-        Bundle bundle = i.getExtras();
-        if (bundle == null) return;
-        for (String key : bundle.keySet()) {
-            Object value = bundle.get(key);
-            Logger.d(TAG, String.format("%s %s (%s)", key,
-                            value.toString(), value.getClass().getName()));
+    private void cancelNotification() {
+        NotificationManager notificationManager =
+            (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.cancel(NOTIFICATION_ID);
+    }
+
+    private void postNotification() {
+        Intent IntentUnmute = new Intent(mContext, EnjoyTheSilenceService.class);
+        IntentUnmute.putExtra("action", "unmute");
+        IntentUnmute.putExtra("systemRingerMode", AudioManager.RINGER_MODE_NORMAL);
+        PendingIntent pIntentUnmute = PendingIntent.getService(mContext, 0, IntentUnmute, 0);
+
+        Notification note  = new Notification.Builder(mContext)
+            .setContentTitle(mContext.getString(R.string.titleSilenceMode))
+            .setContentText(mContext.getString(R.string.msgRestoreRingerMode))
+            .setSmallIcon(R.drawable.ic_launcher)
+            .setContentIntent(pIntentUnmute)
+            .setAutoCancel(true)
+            //.addAction(R.drawable.ic_launcher, "unmute", pIntentUnmute)
+            //.addAction(R.drawable.ic_launcher_gray, "More", pIntentUnmute)
+            .build();
+        note.flags |= Notification.FLAG_NO_CLEAR;
+        note.flags |= Notification.FLAG_ONGOING_EVENT;
+
+        if (Build.VERSION.SDK_INT >= 16) {
+            note.priority = Notification.PRIORITY_HIGH;
         }
+
+        NotificationManager notificationManager =
+            (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(NOTIFICATION_ID, note);
     }
 
 }
